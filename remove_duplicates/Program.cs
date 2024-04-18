6d07060f570fe209e211c05d398aea0cd5871f64
caiovlima <caio_lima_nh@hotmail.com>
2024-04-18 12:28:13 -0300

    :package: up: cresolvendo terceiro problema leetcode

---
+++ remove_duplicates/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace remove_duplicates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0) return 0; // Se o array estiver vazio, não há duplicatas
+
+        int uniqueCount = 1; // Inicialmente, temos pelo menos um número único
+
+        // Percorre o array a partir do segundo elemento
+        for (int i = 1; i < nums.Length; i++)
+        {
+            // Se encontrarmos um número diferente do anterior, atualizamos a próxima posição única
+            if (nums[i] != nums[uniqueCount - 1])
+            {
+                nums[uniqueCount] = nums[i];
+                uniqueCount++;
+            }
+        }
+        return uniqueCount;
+
+    }
+}
